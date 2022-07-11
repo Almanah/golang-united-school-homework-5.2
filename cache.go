f97ab45bcdadbf80f6ef05d9ecd9a52fff645664
Denis Savchyshkin <Denis_Savchyshkin@epam.com>
2022-07-11 13:57:03 +0300

    initial solution

--- cache.go
+++ cache.go
@@ -2,22 +2,60 @@ package cache
 
 import "time"
 
+type Entry struct {
+	value    string
+	deadline time.Time
+}
+
 type Cache struct {
+	entries map[string]Entry
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{entries: make(map[string]Entry)}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (c *Cache) Get(key string) (string, bool) {
+	c.deleteExpiredKeys()
+
+	entry, exists := c.entries[key]
+
+	if !exists {
+		return "", false
+	}
+	return entry.value, true
 
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.deleteExpiredKeys()
+
+	c.entries[key] = Entry{value: value}
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	c.deleteExpiredKeys()
+
+	keys := make([]string, len(c.entries))
+
+	for k := range c.entries {
+		keys = append(keys, k)
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.deleteExpiredKeys()
+
+	c.entries[key] = Entry{value: value, deadline: deadline}
+}
+
+func (c *Cache) deleteExpiredKeys() {
+	now := time.Now()
+
+	for key, entry := range c.entries {
+		if !entry.deadline.IsZero() && entry.deadline.Before(now) {
+			delete(c.entries, key)
+		}
+	}
 }
